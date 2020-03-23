731acdfcd629caae8e40feb37f673ebde007a621
King Rayhan <rayhan095@gmail.com>
2020-03-23 12:53:44 +0600

    new design

--- src/app/MainController.java
+++ src/app/MainController.java
@@ -2,60 +2,38 @@ package app;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-import javax.swing.*;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.util.Optional;
 
 public class MainController {
 
-    private Stage stage = new Stage();
+    FileChooser fc = Utils.FileDialog();
 
     @FXML
-    private VBox window;
+    private TextField comporessorFIleLocationTextField;
 
-    @FXML
-    private FileChooser fc = Utils.FileDialog();
+    public void comporessorChooseSourceFileLocation(ActionEvent e) {
+//        File file = fc.showOpenDialog(new Stage());
+        comporessorFIleLocationTextField.setText("hi");
+    }
 
+    public void comporessorChooseDestinitionFileLocation(ActionEvent e) {
+    }
 
-    public void encodeFile(ActionEvent e) throws IOException {
+    public void extractorChooseSourceFileLocation(ActionEvent e) {
+    }
 
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/app/result.fxml"));
-        Parent result = (Parent) loader.load();
-
-        ResultController rc = loader.getController();
-
-        File file = fc.showOpenDialog(new Stage());
-
-        rc.showFileSize(Integer.toString(Utils.getByte(Utils.getFileContents(file.getCanonicalPath()))));
-
-        window.getChildren().setAll(result);
-
+    public void extractorChooseDestinitionFileLocation(ActionEvent e) {
     }
 
 
-    @FXML
-    public void decodeFile(ActionEvent e) {
+    public void handleCompressButtonClick(ActionEvent e) {
+    }
 
-
-        FileChooser fc = Utils.FileDialog("compressed");
-
-        File file = fc.showSaveDialog(new Stage());
-
-        Utils.saveToFile("Hello world", file);
-
+    public void handleExtractButtonClick(ActionEvent e) {
     }
 
 }
